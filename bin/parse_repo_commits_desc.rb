8641934529ec65e4acd437b56cd6da6b5504a9aa
ts-3156 <ts_3156@yahoo.co.jp>
2019-01-06 07:06:23 +0900

    Add golang data

---
+++ bin/parse_repo_commits_desc.rb
@@ -0,0 +1,30 @@
+require 'csv'
+require 'json'
+
+table = []
+headers = []
+total_count = Hash.new(0)
+title = ARGV[0]
+csv_file = ARGV[1]
+
+CSV.read(csv_file, headers: true).to_a.transpose.each.with_index do |row, i|
+  if i == 0
+    headers = row
+    next
+  end
+
+  obj = {options: {}, data: {}}
+
+  headers.each do |header|
+    if header == 'Name'
+      obj[:options] = {title: {text: title}, subtitle: {text: row[headers.index(header)].to_s}}
+    else
+      total_count[header] += row[headers.index(header)].strip.delete(',').to_i
+      obj[:data][header.strip] = total_count[header]
+    end
+  end
+
+  table << obj
+end
+
+puts JSON.dump(table)
